0b24a69b939b2e48b6bf465073ff0ca7ab73209e
SAMIR-U <samir.borda@uptc.edu.co>
2026-09-21 16:11:31 -0500

    First commit

---
+++ js/sw-utils.js
@@ -0,0 +1,11 @@
+function actualizaCacheDinamico(dynamicCache,
+    req, res) {
+    if (res.ok || res.type === 'opaque') {
+        return caches.open(dynamicCache).then(cache => {
+            cache.put(req, res.clone());
+            return res;
+        });
+    } else {
+        return res;
+    }
+}
